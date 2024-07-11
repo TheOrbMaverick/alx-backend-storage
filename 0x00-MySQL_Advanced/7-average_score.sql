6dd2194b8e0d998247c9d2dddf63cd6c47452934
TheOrbMaverick <enangita@gmail.com>
2024-07-11 15:31:52 +0100

    glam rock

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -6,19 +6,19 @@ DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
 -- Comment: Create the procedure
 DELIMITER //
 
-CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
+CREATE PROCEDURE ComputeAverageScoreForUser(IN in_user_id INT)
 BEGIN
     DECLARE avg_score FLOAT;
 
     -- Comment: Calculate the average score for the given user_id
     SELECT AVG(score) INTO avg_score
     FROM corrections
-    WHERE user_id = user_id;
+    WHERE user_id = in_user_id;
 
     -- Comment: Update the user's average_score in the users table
     UPDATE users
     SET average_score = avg_score
-    WHERE id = user_id;
+    WHERE id = in_user_id;
 END //
 
 DELIMITER ;
