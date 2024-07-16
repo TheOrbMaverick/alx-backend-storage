6991b64066941048f54c876d05098992fdeddc0b
TheOrbMaverick <enangita@gmail.com>
2024-07-16 20:42:57 +0100

    mongo db with python

--- 0x01-NoSQL/102-log_stats.py
+++ 0x01-NoSQL/102-log_stats.py
@@ -10,18 +10,15 @@ if __name__ == "__main__":
     client = MongoClient('mongodb://127.0.0.1:27017')
     collection = client.logs.nginx
 
-    # Count total number of logs
     total_logs = collection.count_documents({})
     print(f"{total_logs} logs")
 
-    # Count the number of logs for each HTTP method
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-    print("Methods:")
+    print('Methods:')
     for method in methods:
         count = collection.count_documents({"method": method})
         print(f"\tmethod {method}: {count}")
 
-    # Count the number of GET requests with path /status
     status_check = collection.count_documents({"method": "GET", "path": "/status"})
     print(f"{status_check} status check")
 
