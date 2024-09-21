acee1ae951e57e1fd9dd2ca529488337887cf7d6
TheOrbMaverick <enangita@gmail.com>
2024-09-21 22:32:09 +0100

    this is the new

--- 0x00-MySQL_Advanced/9-index_name_score.sql
+++ 0x00-MySQL_Advanced/9-index_name_score.sql
@@ -1,4 +1,4 @@
 -- This script creates an index idx_name_first_score on the table names for the first letter of name and the score.
 
 -- Comment: Create the index on the first letter of the name and the score
-CREATE INDEX idx_name_first_score ON names (LEFT(name, 1), score);
+CREATE INDEX idx_name_first_score ON names (name(1), score);
