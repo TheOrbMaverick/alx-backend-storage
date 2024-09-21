b5c58ac1d265d7242e46b547d06067795f26521e
TheOrbMaverick <enangita@gmail.com>
2024-09-21 22:25:32 +0100

    this is the new

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -2,8 +2,16 @@
 -- The metal_bands table is imported, and the lifespan is calculated using the formed and split attributes.
 
 -- Comment: Select bands with Glam rock as their main style and calculate their lifespan
-SELECT band_name, 
-       IFNULL(2022 - formed, 0) AS lifespan
-FROM metal_bands
-WHERE FIND_IN_SET('Glam rock', style) > 0
-ORDER BY lifespan DESC;
+DELIMITER $$
+
+CREATE TRIGGER decrease_quantity_after_order
+AFTER INSERT ON orders
+FOR EACH ROW
+BEGIN
+    -- Decrease the quantity of the ordered item in the items table
+    UPDATE items
+    SET quantity = quantity - NEW.number
+    WHERE name = NEW.item_name;
+END$$
+
+DELIMITER ;
