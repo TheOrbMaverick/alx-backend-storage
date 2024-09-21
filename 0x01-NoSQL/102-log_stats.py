b87f8c74d29cfeef138b636a97ced26691b8d6e4
TheOrbMaverick <enangita@gmail.com>
2024-09-21 22:58:47 +0100

    mongoo

--- 0x01-NoSQL/102-log_stats.py
+++ 0x01-NoSQL/102-log_stats.py
@@ -23,23 +23,29 @@ if __name__ == "__main__":
         print(f"\tmethod {method}: {count}")
 
     # Count the number of GET requests with path /status
-    status_check = collection.count_documents({"method": "GET", "path": "/status"})
+    status_check = collection.count_documents({
+        "method": "GET", "path": "/status"
+    })
     print(f"{status_check} status check")
 
     # Get the top 10 most present IPs
     top_ips = collection.aggregate([
-        {
-            "$group": {
-                "_id": "$ip",
-                "count": {"$sum": 1}
-            }
-        },
+        {"$group":
+         {
+             "_id": "$ip",
+             "count": {"$sum": 1}
+             }},
         {"$sort": {"count": -1}},
-        {"$limit": 10}
+        {"$limit": 10},
+        {"$project":
+            {"ip": "$_id",
+             "count": 1,
+             "_id": 0}}
     ])
 
     # Print the top 10 IPs
     print("IPs:")
-    for ip in top_ips:
-        print(f"\t{ip['_id']}: {ip['count']}")
-
+    for first_ip in top_ips:
+        ip = first_ip.get("ip")
+        count = first_ip.get("count")
+        print(f"\t{ip}: {count}")
