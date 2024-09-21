53be08e79854a97d920ae543369992dc95596874
TheOrbMaverick <enangita@gmail.com>
2024-09-21 22:29:06 +0100

    this is the new

--- 0x00-MySQL_Advanced/8-index_my_names.sql
+++ 0x00-MySQL_Advanced/8-index_my_names.sql
@@ -1,7 +1,4 @@
 -- This script creates an index idx_name_first on the table names for the first letter of name.
 
--- Comment: Drop the index if it exists
-DROP INDEX IF EXISTS idx_name_first ON names;
-
 -- Comment: Create the index on the first letter of the name
 CREATE INDEX idx_name_first ON names (name(1));
--- 0x00-MySQL_Advanced/9-index_name_score.sql
+++ 0x00-MySQL_Advanced/9-index_name_score.sql
@@ -1,7 +1,4 @@
 -- This script creates an index idx_name_first_score on the table names for the first letter of name and the score.
 
--- Comment: Drop the index if it exists
-DROP INDEX IF EXISTS idx_name_first_score ON names;
-
 -- Comment: Create the index on the first letter of the name and the score
 CREATE INDEX idx_name_first_score ON names (LEFT(name, 1), score);
